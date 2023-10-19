f44fdc2307707f9fac0e0c6a1a8b25dddacd7e46
Mariana Silveira <148133489+marisilveirat@users.noreply.github.com>
2023-10-19 19:24:04 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+<h1>Quantas gerações antes de mim?</h1>
+<br>
+<hr>
+<big>Faça o teste aqui embaixo:</big>
+
+<script>
+function pulalinha() {
+	document.write("<br><br>");
+	}
+	
+var idademédia = 28
+var anoatual = 2023
+var anoantigo = 1500
+
+	var diferençaano = 2023 - 1500
+	var mediafinal = diferençaano / idademédia
+	
+	pulalinha()
+	
+document.write("O total de gerações é " + Math.round(mediafinal) + " vezes");
+</script>
